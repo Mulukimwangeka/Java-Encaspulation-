8d92a2c74b7853cb47dc7ffd9e707d5cf77fec0b
mulukimwangeka <mwangekaruth@gmail.com>
2023-03-13 09:31:43 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Person {
+    private String name;
+
+    //getter
+    public String getName() {
+        return name;
+    }
+
+    //setter
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public class Main{
+        public static void main (String[]args){
+            Person person1 = new Person();
+            person1.setName("John");
+            System.out.println(person1.getName());
+
+        }
+    }
+
+}
